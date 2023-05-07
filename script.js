7d8fc57c973c54c1d22ae8dae425c37351a08eba
KawalaE <edyta.wer.kawala@gmail.com>
2023-05-07 21:35:50 +0200

    Create function that pushes info to array

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-const openModalBtn = document.querySelector('.open-modal');
-const modal = document.querySelector('#modal');
-const closeModalBtn = document.querySelector('.close-modal');
+const openModalBtn = document.querySelector(".open-modal");
+const modal = document.querySelector("#modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const submitBtn = document.querySelector(".submit");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const numOfPages = document.querySelector("#num-of-pages");
+const bookStatus = document.querySelector("#status");
 
-openModalBtn.addEventListener('click', ()=> {
+openModalBtn.addEventListener("click", () => {
   modal.showModal();
 });
 
-closeModalBtn.addEventListener('click', () => {
+closeModalBtn.addEventListener("click", () => {
   modal.close();
 });
 
@@ -18,5 +23,22 @@ function Book(title, author, numberOfPages, read) {
   this.numberOfPages = numberOfPages;
   this.read = read;
 }
+const book1 = new Book("Misery", "Stephen King", 310, true);
 
+function addToLibrary(book) {
+  myLibrary.push(Object.values(book));
+}
 
+addToLibrary(book1);
+
+submitBtn.addEventListener("click", () => {
+  const userBook = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    numOfPages.value,
+    bookStatus.checked
+  );
+  addToLibrary(userBook);
+});
+
+console.log(myLibrary);
